ccf358d6a9fc74ee68289012a6fdda10e19cb657
serkanerenn <serkan@yourporter.com>
2020-10-24 10:01:31 +0300

    token handler

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://apirevxray-env.eba-xmaiegrt.us-east-2.elasticbeanstalk.com/',
+  url: 'https://api.hoteluplift.com/',
   authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
 };

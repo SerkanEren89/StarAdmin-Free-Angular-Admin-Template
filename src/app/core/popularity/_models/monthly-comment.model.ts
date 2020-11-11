cc129effc8c7e2b06a3bfce3d2d52ef9e2e08e3a
serkanerenn <serkan@yourporter.com>
2020-11-11 18:29:04 +0300

    popularity

--- src/app/core/popularity/_models/monthly-comment.model.ts
+++ src/app/core/popularity/_models/monthly-comment.model.ts
@@ -3,4 +3,9 @@ import {CommentChannelModel} from './comment-channel.model';
 export class MonthlyCommentModel {
   months: string[];
   item: CommentChannelModel[];
+  currentMonthCount: number;
+  lastMonthCount: number;
+  lastMonthAverage: number;
+  last6MonthCount: number;
+  last6MonthAverage: number;
 }

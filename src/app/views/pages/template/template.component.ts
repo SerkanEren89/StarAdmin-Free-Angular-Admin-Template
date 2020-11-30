4e2e37f221c90d25b29e5dc250c2d8dfc4929c88
serkanerenn <serkan@yourporter.com>
2020-11-30 13:25:31 +0300

    template

--- src/app/views/pages/template/template.component.ts
+++ src/app/views/pages/template/template.component.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs';
 import {TemplateModel} from '../../../core/template/_models/template.model';
 import {TemplateService} from '../../../core/template/_services/template.service';
 import {ToastrService} from 'ngx-toastr';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-template',
@@ -13,11 +14,14 @@ import {ToastrService} from 'ngx-toastr';
 export class TemplateComponent implements OnInit {
   templates$: Observable<TemplateModel[]>;
   templates: TemplateModel[] = [];
+  newTemplate: TemplateModel;
   selectedItem: TemplateModel;
+  closeResult = '';
 
 
   constructor(private templateService: TemplateService,
               private toastr: ToastrService,
+              private modalService: NgbModal,
               private cdr: ChangeDetectorRef) {
   }
 
@@ -30,6 +34,7 @@ export class TemplateComponent implements OnInit {
       } else {
         this.selectedItem = new TemplateModel();
       }
+      this.newTemplate = new TemplateModel();
       this.cdr.detectChanges();
     });
   }
@@ -41,11 +46,31 @@ export class TemplateComponent implements OnInit {
   save(template: TemplateModel) {
     this.templateService.saveTemplates(template)
       .subscribe((templateModel: TemplateModel) => {
+        this.selectedItem = templateModel;
+        this.modalService.dismissAll();
         this.toastr.success('Your template saved successfully');
       });
   }
 
   addNew() {
     this.selectedItem = new TemplateModel();
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', scrollable: true}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }

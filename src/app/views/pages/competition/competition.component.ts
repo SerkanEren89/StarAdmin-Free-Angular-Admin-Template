ce32882ad04391ca96490aaa1423ea348b134f10
serkanerenn <serkan@yourporter.com>
2020-12-14 18:55:54 +0300

    some fix list

--- src/app/views/pages/competition/competition.component.ts
+++ src/app/views/pages/competition/competition.component.ts
@@ -4,6 +4,8 @@ import {CompetitionService} from '../../../core/competition/_services/competitio
 import {CompetitionCountRatingModel} from '../../../core/competition/_models/competition-count-rating.model';
 import {CategoryGroupModel} from '../../../core/category/_models/category-group.model';
 import {TableService} from '../../../core/general/_services/table.service';
+import {HotelModel} from '../../../core/hotel/_models/hotel.model';
+import {HotelService} from '../../../core/hotel/_services/hotel.service';
 
 @Component({
   selector: 'app-competition',
@@ -15,30 +17,34 @@ export class CompetitionComponent implements OnInit {
   @ViewChild('competitionTable') competitionRef;
   @ViewChild('rankingTable') rankingRef;
   @ViewChild('pricingTable') pricingRef;
-  competitionCountRatingList$: Observable<CompetitionCountRatingModel[]>;
   competitionCountRatingList: CompetitionCountRatingModel[];
-  competitionCategoryList$: Observable<CategoryGroupModel[]>;
   competitionCategoryList: CategoryGroupModel[];
   competitionCategory$: Observable<CategoryGroupModel[]>;
   competitionCategory: CategoryGroupModel[] = [];
+  competitionList: HotelModel[];
   selectedCategory: CategoryGroupModel;
 
   constructor(private competitionService: CompetitionService,
+              private hotelService: HotelService,
               private tableService: TableService,
               private cdr: ChangeDetectorRef) {
   }
 
   ngOnInit() {
-    this.competitionCountRatingList$ = this.competitionService.getCompetitionCountRatingList();
-    this.competitionCountRatingList$.subscribe((competitionCountRatingList: CompetitionCountRatingModel[]) => {
+    this.hotelService.getCompetitors()
+      .subscribe((competitionList: HotelModel[]) => {
+        this.competitionList = competitionList;
+      })
+    this.competitionService.getCompetitionCountRatingList()
+      .subscribe((competitionCountRatingList: CompetitionCountRatingModel[]) => {
       this.competitionCountRatingList = competitionCountRatingList;
       this.cdr.detectChanges();
       this.tableService.addLabelTag(this.competitionRef);
       this.tableService.addLabelTag(this.rankingRef);
       this.tableService.addLabelTag(this.pricingRef);
     });
-    this.competitionCategoryList$ = this.competitionService.getCompetitionCategoryList();
-    this.competitionCategoryList$.subscribe((competitionCategoryList: CategoryGroupModel[]) => {
+    this.competitionService.getCompetitionCategoryList()
+      .subscribe((competitionCategoryList: CategoryGroupModel[]) => {
       this.competitionCategoryList = competitionCategoryList;
       this.cdr.detectChanges();
     });
